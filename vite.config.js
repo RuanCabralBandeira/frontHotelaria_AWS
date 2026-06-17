56ca3b0501477d8f4f3fc2b71182299bfa10f85e
ruanCabralBandeira <iffygorilla29@gmail.com>
2026-06-17 11:00:47 -0300

    claude

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/20261prj5/hotel/',
+  base: '20261prj5/hotel/',
   server: {
     port: 9540,
     host: true
